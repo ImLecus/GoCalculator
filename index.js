352fb26e6ab945d2901bb66fc07dd5ae25506911
ImLecus <marcosgf2005@gmail.com>
2022-10-26 18:12:57 +0200

    added previous operation

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var result = "";
 var compiledResult = "";
+var previousResult = "";
 
 const Refresh = () => {
   document.getElementById("result").innerHTML = result; 
+  document.getElementById("operation").innerHTML = previousResult; 
   if(result == ""){
     document.getElementById("result").innerHTML = "0"; 
   }
 }
 Array.from(document.querySelectorAll(".btn")).forEach(button => {
+  previousResult = result;
   button.addEventListener("click", () => {
     result += button.innerHTML;
     Refresh();
@@ -18,10 +21,12 @@ document.querySelector(".del").addEventListener("click", () => {
   Refresh();
 });
 document.querySelector(".ac").addEventListener("click", () => {
+  previousResult = "";
   result = "";
   Refresh();
 });
 document.querySelector(".enter").addEventListener("click", () => {
+  previousResult = result;
   compiledResult = result.replace("÷","/");
   compiledResult = compiledResult.replace("×","*");
   result = eval(compiledResult).toString();
